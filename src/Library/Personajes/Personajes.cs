d910a786b2930ee8862f61378fdef5dba4c4d87c
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2024-09-15 14:31:29 -0300

    quité los métodos de atacar, defender y curar para agregarlos en la clase combate

--- src/Library/Personajes/Personajes.cs
+++ src/Library/Personajes/Personajes.cs
@@ -12,7 +12,7 @@ public class Personaje
         this.Nombre = nombre;
         this.Vida = vidaInicial;
         this.VidaInicial = vidaInicial;
-        this.Items = new List<Item>()
+        this.Items = new List<Item>();
 
     }
     // Método para agregar item tanto de ataque como de defensa
@@ -38,38 +38,4 @@ public class Personaje
     {
         return Item.ObtenerValorDefensa(Items); // Implementado en Clase Items
     }
-    
-    // Metodo de ataque a otro personaje
-
-    public void Atacar(Personaje objetivo)
-    {
-        int daño = this.ObtenerValorAtaque(); // El daño de ataque es el valor de ataque del personaje que ataca
-        objetivo.Defensa(daño); // Le pasa el valor de dano a la defensa del objetivo para ver cuanto le saca de vida
-    }
-    public void Defensa(int daño)
-    {
-        int defensa = this.ObtenerValorDefensa(); // El daño de ataque es el valor de ataque del personaje que ataca
-        int dañoFinal = daño - defensa; // Resta el daño a la defensa del atacado y asigna el nuevo dano
-        
-        //Evaluamos si la defensa fue mayor a da;o de ser asi, no resta nada a la vida
-        
-        if (dañoFinal < 0)
-        {
-            dañoFinal = 0;
-        }
-
-        objetivo.Vida -= dañoFinal; // Asignar nueva vida restandole el da;o final
-        
-        if (objetivo.Vida <= 0)
-        {
-            objetivo.Vida = 0;
-        }
-    }
-//Metodo para reiniciar la vida a su vida inicial
-    public void Curar()
-    {
-        this.Vida = this.VidaInicial;
-    }
-    
-    
 }
