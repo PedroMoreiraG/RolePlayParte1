bcc0e03774fafbc1cd970fca09f9c26713326af3
Lucas-BTNCR <Lucasbentancour@outlook.com>
2024-09-15 21:44:10 -0300

    PJ3, enano

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,12 +6,23 @@ class Program
     {
         Personaje pj1 = new Personaje("Luis", "Mago");
         Personaje pj2 = new Personaje("Edison", "Guerrero");
+        Personaje pj3 = new Personaje("Lugano", "Enano");
+
+
         Personaje error = new Personaje("Diego", "Error");
         
         Item item1 = new Item("Espada", 150, 0);
         Item item2 = new Item("Escudo", 0, 40);
+
+
+        Item item5 = new Item("Armadura", 0, 50);
+        Item item6 = new Item("Hacha", 100, 0);
         item1.agregar_item(pj1);
         item2.quitar_item(pj1);
+        
+        
+        item5.agregar_item(pj3);
+        item6.quitar_item(pj3);     
 
         Combate combate = new Combate();
         combate.atacar_personaje(pj1,pj2);
--- test/LibraryTests/ChequeoRazasTests.cs
+++ test/LibraryTests/ChequeoRazasTests.cs
@@ -1,8 +1,50 @@
-namespace LibraryTests;
+using NUnit.Framework;
+using Library;
 
-[TestFixture]
-public class ChequeoRazasTests
+namespace LibraryTests
 {
-    [Test]
-    [TestCase()]
+    public class ChequeoRazasTests
+    {
+        // Test genérico para todas las razas válidas
+        [TestCase("Mago", 70, 30)]
+        [TestCase("Enano", 50, 60)]
+        [TestCase("Elfo", 55, 55)]
+        [TestCase("Guerrero", 60, 50)]
+        [TestCase("Hada", 65, 45)]
+        public void Estadistica_DeberiaAsignarEstadisticas_Correctamente(string raza, int ataqueEsperado, int defensaEsperada)
+        {
+            // Arrange
+            var chequeoRazas = new ChequeoRazas(); // Inicializado dentro del test
+            var personaje = new Personaje("NombrePrueba", raza);
+
+            // Act
+            chequeoRazas.Estadistica(personaje);
+
+            // Assert
+            Assert.AreEqual(ataqueEsperado, personaje.Ataque, $"Ataque esperado para raza {raza}");
+            Assert.AreEqual(defensaEsperada, personaje.Defensa, $"Defensa esperada para raza {raza}");
+        }
+
+        // Test para raza desconocida
+        [Test]
+        public void Estadistica_DeberiaMostrarMensajeParaRazaDesconocida()
+        {
+            // Arrange
+            var chequeoRazas = new ChequeoRazas(); // Inicializado dentro del test
+            var personaje = new Personaje("Desconocido", "Alien");
+
+            // Capturar la salida de consola
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                // Act
+                chequeoRazas.Estadistica(personaje);
+
+                // Assert
+                var expectedMessage = $"La raza del personaje llamado {personaje.Nombre} no existe\r\n";
+                Assert.AreEqual(expectedMessage, sw.ToString());
+            }
+        }
+    }
 }
